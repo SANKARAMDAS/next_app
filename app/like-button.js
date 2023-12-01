0981f64a47945841ca8330fcc0932ea016eeba14
SAN.D <das95sankaram@gmail.com>
2023-12-01 16:50:23 +0530

    next-js auth

--- app/like-button.js
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useState} from "react";
-
-export default function LikeButton(){
-    let [isLiked, setLiked] = useState(false);
-    return (<div>
-        <button onClick={()=> setLiked(!isLiked)}>{isLiked? 'Unliked' : 'Like'}</button>
-    </div>)
-}
